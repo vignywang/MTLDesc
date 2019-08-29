f7bc1af974cba142e15d3464a3d4cc153b6e42a6
张宇阳 <zhangyuyang@megvii.com>
2019-08-29 11:33:08 +0800

    [Bug Fix] 修正了文件夹命名的问题

--- utils/adaption_maker.py
+++ utils/adaption_maker.py
@@ -58,11 +58,15 @@ class AdaptionMaker(object):
         self.val_out_dir = os.path.join(self.out_root, 'val2014', 'pseudo_image_points')
 
         count = 1
-        while os.path.exists(self.train_out_dir):
-            self.train_out_dir += '%d' % count
-            self.val_out_dir += '%d' % count
+        train_out_dir = self.train_out_dir
+        val_out_dir = self.val_out_dir
+        while os.path.exists(train_out_dir):
+            train_out_dir = self.train_out_dir + '%d' % count
+            val_out_dir = self.val_out_dir + '%d' % count
             count += 1
 
+        self.train_out_dir = train_out_dir
+        self.val_out_dir = val_out_dir
         # self.train_out_dir += '_debug'
         # self.val_out_dir += '_debug'
 
