aa437cd3ca927834b8710b65907d291bba47c7c9
张宇阳 <zhangyuyang@megvii.com>
2019-09-04 17:12:49 +0800

    [Code Improve] 调整Tester的任务为测试其他方法的相关结果
    Tester不负责模型的测试

--- utils/testers.py
+++ utils/testers.py
@@ -6,140 +6,11 @@ import time
 import cv2 as cv
 import numpy as np
 import torch
-import torch.nn.functional as f
 
-from nets.superpoint_net import SuperPointNet
-from data_utils.synthetic_dataset import SyntheticValTestDataset
 from data_utils.hpatch_dataset import HPatchDataset
-from utils.evaluation_tools import mAPCalculator, RepeatabilityCalculator, HomoAccuracyCalculator, MeanMatchingAccuracy
+from utils.evaluation_tools import RepeatabilityCalculator, HomoAccuracyCalculator, MeanMatchingAccuracy
 from utils.tranditional_algorithm import FAST
 from utils.utils import spatial_nms, Matcher
-
-
-class MagicPointSyntheticTester(object):
-
-    def __init__(self, params):
-        self.params = params
-        self.logger = params.logger
-        self.save_threshold_curve = self.params.save_threshold_curve
-        if torch.cuda.is_available():
-            self.logger.info('gpu is available, set device to cuda!')
-            self.device = torch.device('cuda:0')
-        else:
-            self.logger.info('gpu is not available, set device to cpu!')
-            self.device = torch.device('cpu')
-
-        # 初始化测试数据集
-        test_dataset = SyntheticValTestDataset(params, dataset_type='validation', add_noise=True)
-
-        # 初始化模型
-        model = SuperPointNet()
-
-        # 初始化测评计算子
-        mAP_calculator = mAPCalculator()
-
-        self.test_dataset = test_dataset
-        self.test_length = len(test_dataset)
-        self.model = model
-        self.mAP_calculator = mAP_calculator
-
-    def test(self, ckpt_file):
-
-        if ckpt_file == None:
-            print("Please input correct checkpoint file dir!")
-            return
-
-        curve_name = None
-        curve_dir = None
-        if self.save_threshold_curve:
-            save_root = '/'.join(ckpt_file.split('/')[:-1])
-            curve_name = (ckpt_file.split('/')[-1]).split('.')[0]
-            curve_dir = os.path.join(save_root, curve_name + '.png')
-
-        # 从预训练的模型中恢复参数
-        model_dict = self.model.state_dict()
-        pretrain_dict = torch.load(ckpt_file)
-        model_dict.update(pretrain_dict)
-        self.model.load_state_dict(model_dict)
-        self.model.to(self.device)
-
-        # 重置测评算子参数
-        self.mAP_calculator.reset()
-
-        self.model.eval()
-
-        self.logger.info("*****************************************************")
-        self.logger.info("Testing model %s" % ckpt_file)
-
-        start_time = time.time()
-        count = 0
-
-        for i, data in enumerate(self.test_dataset):
-            image = data['image']
-            gt_point = data['gt_point']
-            gt_point = gt_point.numpy()
-
-            image = image.to(self.device).unsqueeze(dim=0)
-            # 得到原始的经压缩的概率图，概率图每个通道64维，对应空间每个像素是否为关键点的概率
-            _, _, prob = self.model(image)
-            # 将概率图展开为原始图像大小
-            prob = f.pixel_shuffle(prob, 8)
-            # 进行非极大值抑制
-            prob = spatial_nms(prob)
-            prob = prob.detach().cpu().numpy()[0, 0]
-
-            self.mAP_calculator.update(prob, gt_point)
-            if i % 10 == 0:
-                print("Having tested %d samples, which takes %.3fs" % (i, (time.time()-start_time)))
-                start_time = time.time()
-            count += 1
-            # if count % 1000 == 0:
-            #     break
-
-        mAP, test_data = self.mAP_calculator.compute_mAP()
-        if self.save_threshold_curve:
-            self.mAP_calculator.plot_threshold_curve(test_data, curve_name, curve_dir)
-
-        self.logger.info("The mean Average Precision : %.4f of %d samples" % (mAP, count))
-        self.logger.info("Testing done.")
-        self.logger.info("*****************************************************")
-
-    def test_single_image(self, ckpt_file, image_dir):
-
-        if ckpt_file == None:
-            print("Please input correct checkpoint file dir!")
-            return
-
-        # 从预训练的模型中恢复参数
-        model_dict = self.model.state_dict()
-        pretrain_dict = torch.load(ckpt_file, map_location=self.device)
-        model_dict.update(pretrain_dict)
-        self.model.load_state_dict(model_dict)
-        self.model.to(self.device)
-
-        self.model.eval()
-
-        cv_image = cv.imread(image_dir, cv.IMREAD_GRAYSCALE)
-        image = np.expand_dims(np.expand_dims(cv_image, 0), 0)
-        image = torch.from_numpy(image).to(torch.float)
-
-        _, _, prob = self.model(image)
-        prob = f.pixel_shuffle(prob, 8)
-        # 进行非极大值抑制
-        prob = spatial_nms(prob)
-        prob = prob.detach().cpu().numpy()[0, 0]
-
-        pred_pt = np.where(prob>0.1)
-
-        cv_pt_list = []
-        for i in range(len(pred_pt[0])):
-            kpt = cv.KeyPoint()
-            kpt.pt = (pred_pt[1][i], pred_pt[0][i])
-            cv_pt_list.append(kpt)
-
-        result_dir = os.path.join('/'.join(ckpt_file.split('/')[:-1]), 'test_image.jpg')
-        cv_image = cv.drawKeypoints(cv_image, cv_pt_list, None, color=(0, 0, 255))
-        cv.imwrite(result_dir, cv_image)
 
 
 class HPatchTester(object):
@@ -151,19 +22,12 @@ class HPatchTester(object):
         self.correct_epsilon = params.correct_epsilon
         self.top_k = params.top_k
         self.nms_threshold = params.nms_threshold
-        if torch.cuda.is_available():
-            self.logger.info('gpu is available, set device to cuda!')
-            self.device = torch.device('cuda:0')
-        else:
-            self.logger.info('gpu is not available, set device to cpu!')
-            self.device = torch.device('cpu')
 
         # 初始化测试数据集
         self.test_dataset = HPatchDataset(params)
         self.test_length = len(self.test_dataset)
 
         # 初始化模型
-        self.model = SuperPointNet()
         self.model_fast = FAST(top_k=self.top_k)
         self.orb = cv.ORB_create(nfeatures=self.top_k)
 
@@ -226,117 +90,6 @@ class HPatchTester(object):
 
         self.logger.info("FAST Repeatability: illumination: %.4f, viewpoint: %.4f, total: %.4f, %d/%d" %
                          (illum_repeat, view_repeat, total_repeat, count, len(self.test_dataset)))
-        self.logger.info("Testing HPatch Repeatability done.")
-        self.logger.info("*****************************************************")
-
-    def test_model(self, ckpt_file):
-
-        if ckpt_file == None:
-            print("Please input correct checkpoint file dir!")
-            return
-
-        # 从预训练的模型中恢复参数
-        model_dict = self.model.state_dict()
-        pretrain_dict = torch.load(ckpt_file)
-        model_dict.update(pretrain_dict)
-        self.model.load_state_dict(model_dict)
-        self.model.to(self.device)
-
-        # 重置测评算子参数
-        self.illumination_repeatability.reset()
-        self.illumination_homo_accuracy.reset()
-        self.illumination_mma.reset()
-        self.viewpoint_repeatability.reset()
-        self.viewpoint_homo_accuracy.reset()
-        self.viewpoint_mma.reset()
-
-        self.model.eval()
-
-        self.logger.info("*****************************************************")
-        self.logger.info("Testing model %s" % ckpt_file)
-
-        start_time = time.time()
-        count = 0
-
-        for i, data in enumerate(self.test_dataset):
-            first_image = data['first_image']
-            second_image = data['second_image']
-            gt_homography = data['gt_homography']
-            image_type = data['image_type']
-
-            image_pair = np.stack((first_image, second_image), axis=0)
-            # image_pair = torch.from_numpy(image_pair).to(torch.float).to(self.device).unsqueeze(dim=1)/255.
-            image_pair = torch.from_numpy(image_pair).to(torch.float).to(self.device).unsqueeze(dim=1)
-
-            _, desp_pair, prob_pair = self.model(image_pair)
-            prob_pair = f.pixel_shuffle(prob_pair, 8)
-            prob_pair = spatial_nms(prob_pair, kernel_size=int(self.nms_threshold*2+1))
-
-            desp_pair = desp_pair.detach().cpu().numpy()
-            first_desp = desp_pair[0]
-            second_desp = desp_pair[1]
-            prob_pair = prob_pair.detach().cpu().numpy()
-            first_prob = prob_pair[0, 0]
-            second_prob = prob_pair[1, 0]
-
-            # 得到对应的预测点
-            first_point = self.generate_predict_point(first_prob, top_k=self.top_k)  # [n,2]
-            second_point = self.generate_predict_point(second_prob, top_k=self.top_k)  # [m,2]
-
-            # 得到点对应的描述子
-            select_first_desp = self.generate_predict_descriptor(first_point, first_desp)
-            select_second_desp = self.generate_predict_descriptor(second_point, second_desp)
-
-            # 得到匹配点
-            matched_point = self.matcher(first_point, select_first_desp,
-                                         second_point, select_second_desp)
-
-            # 计算得到单应变换
-            pred_homography, _ = cv.findHomography(matched_point[0][:, np.newaxis, ::-1],
-                                                   matched_point[1][:, np.newaxis, ::-1], cv.RANSAC)
-
-            # 对单样本进行测评
-            if image_type == 'illumination':
-                self.illumination_repeatability.update(first_point, second_point, gt_homography)
-                self.illumination_homo_accuracy.update(pred_homography, gt_homography)
-                self.illumination_mma.update(gt_homography, matched_point)
-            elif image_type == 'viewpoint':
-                self.viewpoint_repeatability.update(first_point, second_point, gt_homography)
-                self.viewpoint_homo_accuracy.update(pred_homography, gt_homography)
-                self.viewpoint_mma.update(gt_homography, matched_point)
-            else:
-                print("The image type magicpoint_tester.test(ckpt_file)must be one of illumination of viewpoint ! "
-                      "Please check !")
-                assert False
-
-            if i % 10 == 0:
-                print("Having tested %d samples, which takes %.3fs" % (i, (time.time()-start_time)))
-                start_time = time.time()
-            count += 1
-            # if count % 1000 == 0:
-            #     break
-
-        # 计算各自的重复率以及总的重复率
-        illum_repeat, illum_repeat_sum, illum_num_sum = self.illumination_repeatability.average()
-        view_repeat, view_repeat_sum, view_num_sum = self.viewpoint_repeatability.average()
-        total_repeat = (illum_repeat_sum + view_repeat_sum) / (illum_num_sum + view_num_sum)
-
-        # 计算估计的单应变换准确度
-        illum_homo_acc, illum_homo_sum, illum_homo_num = self.illumination_homo_accuracy.average()
-        view_homo_acc, view_homo_sum, view_homo_num = self.viewpoint_homo_accuracy.average()
-        total_homo_acc = (illum_homo_sum + view_homo_sum) / (illum_homo_num + view_homo_num)
-
-        # 计算匹配的准确度
-        illum_match_acc, illum_match_sum, illum_match_num = self.illumination_mma.average()
-        view_match_acc, view_match_sum, view_match_num = self.viewpoint_mma.average()
-        total_match_acc = (illum_match_sum + view_match_sum) / (illum_match_num + view_match_num)
-
-        self.logger.info("Homography accuracy: illumination: %.4f, viewpoint: %.4f, total: %.4f" %
-                         (illum_homo_acc, view_homo_acc, total_homo_acc))
-        self.logger.info("Mean Matching Accuracy: illumination: %.4f, viewpoint: %.4f, total: %.4f " %
-                         (illum_match_acc, view_match_acc, total_match_acc))
-        self.logger.info("Repeatability: illumination: %.4f, viewpoint: %.4f, total: %.4f" %
-                         (illum_repeat, view_repeat, total_repeat))
         self.logger.info("Testing HPatch Repeatability done.")
         self.logger.info("*****************************************************")
 
@@ -422,7 +175,7 @@ class HPatchTester(object):
                          (illum_match_acc, view_match_acc, total_match_acc))
         self.logger.info("Repeatability: illumination: %.4f, viewpoint: %.4f, total: %.4f" %
                          (illum_repeat, view_repeat, total_repeat))
-        self.logger.info("Testing HPatch Repeatability done.")
+        self.logger.info("Testing HPatch done.")
         self.logger.info("*****************************************************")
 
     def generate_predict_point(self, prob, scale=None, top_k=0):
