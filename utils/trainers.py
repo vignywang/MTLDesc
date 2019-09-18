cb7a715fb98e9ef811781e04c082e12197708b29
张宇阳 <zhangyuyang@megvii.com>
2019-09-18 19:10:01 +0800

    [Code Improve] 给summary_writer中加入epoch_idx

--- utils/trainers.py
+++ utils/trainers.py
@@ -1129,12 +1129,12 @@ class SuperPoint(TrainerTester):
                          (illum_repeat, view_repeat, total_repeat))
         self.logger.info("Detection point, average: %.4f, variance: %.4f" % (point_avg, point_std))
 
-        self.summary_writer.add_scalar("illumination/Homography_Accuracy", illum_homo_acc)
-        self.summary_writer.add_scalar("illumination/Mean Matching_Accuracy", illum_match_acc)
-        self.summary_writer.add_scalar("illumination/Repeatability", illum_repeat)
-        self.summary_writer.add_scalar("viewpoint/Homography_Accuracy", view_homo_acc)
-        self.summary_writer.add_scalar("viewpoint/Mean Matching_Accuracy", view_match_acc)
-        self.summary_writer.add_scalar("viewpoint/Repeatability", view_repeat)
+        self.summary_writer.add_scalar("illumination/Homography_Accuracy", illum_homo_acc, epoch_idx)
+        self.summary_writer.add_scalar("illumination/Mean Matching_Accuracy", illum_match_acc, epoch_idx)
+        self.summary_writer.add_scalar("illumination/Repeatability", illum_repeat, epoch_idx)
+        self.summary_writer.add_scalar("viewpoint/Homography_Accuracy", view_homo_acc, epoch_idx)
+        self.summary_writer.add_scalar("viewpoint/Mean Matching_Accuracy", view_match_acc, epoch_idx)
+        self.summary_writer.add_scalar("viewpoint/Repeatability", view_repeat, epoch_idx)
 
     def _test_model_both(self, epoch_idx):
 
