ea51047794fe6fd5c53c53803b92dfc36eb42116
张宇阳 <zhangyuyang@megvii.com>
2019-10-18 10:59:19 +0800

    [Code Develop] 主要是修改了EncoderDecoder的网络结构以满足训练需求

--- nets/megpoint_net.py
+++ nets/megpoint_net.py
@@ -306,8 +306,9 @@ class Discriminator(nn.Module):
 
 class EncoderDecoderMegPoint(nn.Module):
 
-    def __init__(self):
+    def __init__(self, only_detector=False):
         super(EncoderDecoderMegPoint, self).__init__()
+        self.only_detector = only_detector
         self.relu = nn.ReLU(inplace=True)
         self.tanh = nn.Tanh()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -332,10 +333,26 @@ class EncoderDecoderMegPoint(nn.Module):
         self.upconv2a = nn.Conv2d(c2, c2, kernel_size=3, stride=1, padding=1)
         self.upconv2b = nn.Conv2d(c2, c1, kernel_size=3, stride=1, padding=1)
         self.upconv1a = nn.Conv2d(c1, c1, kernel_size=3, stride=1, padding=1)
-        self.upconv1b = nn.Conv2d(c1, 1, kernel_size=3, stride=1, padding=1)
+        self.upconv1b = nn.Conv2d(c1, 2, kernel_size=3, stride=1, padding=1)
+
+        self.soffmax = nn.Softmax(dim=1)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                if m.affine:
+                    nn.init.constant_(m.weight, 1)
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
 
+        _, _, h, w = x.shape
         # encoder part
         x = self.relu(self.conv1a(x))
         x = self.relu(self.conv1b(x))
@@ -352,25 +369,28 @@ class EncoderDecoderMegPoint(nn.Module):
         feature = self.relu(self.conv4b(x))
 
         # decoder part
-        ux = f.interpolate(feature, scale_factor=2, mode='bilinear', align_corners=False)
+        ux = f.interpolate(feature, scale_factor=2, mode='bilinear', align_corners=True)
         ux = self.relu(self.upconv3a(ux))
         ux = self.relu(self.upconv3b(ux))
 
-        ux = f.interpolate(ux, scale_factor=2, mode='bilinear', align_corners=False)
+        ux = f.interpolate(ux, scale_factor=2, mode='bilinear', align_corners=True)
         ux = self.relu(self.upconv2a(ux))
         ux = self.relu(self.upconv2b(ux))
 
-        ux = f.interpolate(ux, scale_factor=2, mode='bilinear', align_corners=False)
-        up_feature = self.relu(self.upconv1a(ux))
-        recovered_image = self.tanh(self.upconv1b(up_feature))
+        ux = f.interpolate(ux, scale_factor=2, mode="bilinear", align_corners=True)
+        ux = self.relu(self.upconv1a(ux))
+        logit = self.upconv1b(ux)
+        prob = self.soffmax(logit)
 
-        # descriptor head
-        cDa = self.relu(self.convDa(feature))
-        descriptor = self.convDb(cDa)
-        dn = torch.norm(descriptor, p=2, dim=1, keepdim=True)
-        descriptor = descriptor.div(dn)
-
-        return recovered_image, descriptor, up_feature
+        if not self.only_detector:
+            # descriptor head
+            cDa = self.relu(self.convDa(feature))
+            descriptor = self.convDb(cDa)
+            dn = torch.norm(descriptor, p=2, dim=1, keepdim=True)
+            descriptor = descriptor.div(dn)
+            return logit, prob, descriptor
+        else:
+            return logit, prob
 
 
 class HardDetectionModule(nn.Module):
