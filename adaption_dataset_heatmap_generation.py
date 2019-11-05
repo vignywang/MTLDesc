d7b33f362909c0cdca09b4c56f63e779e5c5174f
张宇阳 <zhangyuyang@megvii.com>
2019-11-05 15:30:02 +0800

    [Params Change] 更改预训练的模型，调整检测的参数

--- adaption_dataset_heatmap_generation.py
+++ adaption_dataset_heatmap_generation.py
@@ -20,13 +20,12 @@ class HeatmapAdaptionParameters(object):
     def __init__(self):
         self.dataset_dir = "/data/MegPoint/dataset/coco"
         self.out_dir = "/data/MegPoint/dataset/coco/train2014/heatmap_image_pseudo_point_00"
-        self.ckpt_file = "/home/zhangyuyang/project/development/MegPoint/magicpoint_ckpt/synthetic_heatmap_l2_0.0010_32/model_76.pt"
+        self.ckpt_file = "/home/zhangyuyang/project/development/MegPoint/magicpoint_ckpt/synthetic_heatmap_shuffle_weight_0.0010_64/model_59.pt"
         self.height = 240
         self.width = 320
         self.batch_size = 24
         self.adaption_num = 100
-        self.point_portion = 0.001
-        self.detection_threshold = 0.5
+        self.detection_threshold = 0.4
         self.nms_threshold = 4
         self.top_k = 300
         self.point_threshold = None
