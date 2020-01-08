65bcd38a15e6893b37b3da79545ef9e44b426632
张宇阳 <zhangyuyang@megvii.com>
2020-01-08 10:06:19 +0800

    [Code Develop] 新增coco数据集类用于训练模型ResNetAll

--- data_utils/coco_dataset.py
+++ data_utils/coco_dataset.py
@@ -860,7 +860,7 @@ class COCOMegPointDescriptorOnlyDataset(Dataset):
 
         return point
 
-    def _generate_fixed_grid(self, x_num=30, y_num=30):
+    def _generate_fixed_grid(self, x_num=20, y_num=20):
         """
         预先采样固定间隔的225个图像格子
         """
@@ -908,6 +908,254 @@ class COCOMegPointDescriptorOnlyDataset(Dataset):
         image_list = sorted(image_list)
 
         return image_list
+
+
+class COCOMegPointHeatmapAllTrainDataset(Dataset):
+    """
+    根据新的网络模型ResNetAll及HalfResNetAll设计的coco数据集类
+    """
+
+    def __init__(self, params):
+        self.params = params
+        self.height = params.height
+        self.width = params.width
+        assert self.height == 240 and self.width == 320
+
+        self.dataset_dir = params.dataset_dir
+        # self.params.logger.info("Initialize MegPoint Train Dataset: %s" % self.dataset_dir)
+        self.image_list, self.point_list = self._format_file_list()
+
+        self.homography = HomographyAugmentation()
+        self.photometric = PhotometricAugmentation()
+
+        self.fix_grid = self._generate_fixed_grid()
+
+    def __len__(self):
+        assert len(self.image_list) == len(self.point_list)
+        return len(self.image_list)
+
+    def __getitem__(self, idx):
+        """
+        Returns:
+            image: [1,h,w] 归一化到[-1,1]之间的图像
+            point_mask: [h,w] 每个点代表此处是否有效
+            heatmap: [h,w] 有关键点的位置为1，否则为0
+            warped_image: [1,h,w]
+            warped_point_mask: [h,w]
+            warped_heatmap: [h,w]
+            desp_point: [n,1,2] 用于训练描述子随机采样的坐标点
+            warped_desp_point: [n,1,2] 与desp_point对应的在变换图像中的采样点
+            valid_mask: [n] 表示这些坐标点的有效性
+            not_search_mask: [n,n] 用于构造loss时负样本不搜索的区域
+        """
+        image = cv.imread(self.image_list[idx], flags=cv.IMREAD_GRAYSCALE)
+        point = np.load(self.point_list[idx])
+        point_mask = np.ones_like(image).astype(np.float32)
+
+        # 1、由随机采样的单应变换得到第二副图像及其对应的关键点位置、原始掩膜和该单应变换
+        if torch.rand([]).item() < 0.5:
+            warped_image, warped_point_mask, warped_point, homography = \
+                image.copy(), point_mask.copy(), point.copy(), np.eye(3)
+        else:
+            warped_image, warped_point_mask, warped_point, homography = self.homography(image, point, return_homo=True)
+
+        if torch.rand([]).item() < 0.5:
+            image = self.photometric(image)
+            warped_image = self.photometric(warped_image)
+
+        # 2.1 得到第一副图点构成的热图
+        heatmap = self._convert_points_to_heatmap(point)
+
+        # 2.2 得到第二副图点构成的热图
+        warped_heatmap = self._convert_points_to_heatmap(warped_point)
+
+        # 3、采样训练描述子要用的点
+        desp_point = self._random_sample_point()
+        height, width = image.shape
+
+        warped_desp_point, valid_mask, not_search_mask = self._generate_warped_point(
+            desp_point, homography, height, width)
+
+        # debug use
+        # image_point = draw_image_keypoints(image, desp_point, show=False)
+        # warped_image_point = draw_image_keypoints(warped_image, warped_desp_point, show=False)
+        # cat_all = np.concatenate((image_point, warped_image_point), axis=1)
+        # cv.imshow("cat_all", cat_all)
+        # cv.waitKey()
+
+        image = image.astype(np.float32) * 2. / 255. - 1.
+        warped_image = warped_image.astype(np.float32) * 2. / 255. - 1.
+
+        image = torch.from_numpy(image).unsqueeze(dim=0)
+        warped_image = torch.from_numpy(warped_image).unsqueeze(dim=0)
+
+        point_mask = torch.from_numpy(point_mask)
+        warped_point_mask = torch.from_numpy(warped_point_mask)
+
+        desp_point = torch.from_numpy(self._scale_point_for_sample(desp_point))
+        warped_desp_point = torch.from_numpy(self._scale_point_for_sample(warped_desp_point))
+
+        valid_mask = torch.from_numpy(valid_mask)
+        not_search_mask = torch.from_numpy(not_search_mask)
+
+        return {
+            "image": image,  # [1,h,w]
+            "point_mask": point_mask,  # [h,w]
+            "heatmap": heatmap,  # [h,w]
+            "warped_image": warped_image,  # [1,h,w]
+            "warped_point_mask": warped_point_mask,  # [h,w]
+            "warped_heatmap": warped_heatmap,  # [h,w]
+            "desp_point": desp_point,  # [n,1,2]
+            "warped_desp_point": warped_desp_point,  # [n,1,2]
+            "valid_mask": valid_mask,  # [n]
+            "not_search_mask": not_search_mask,  # [n,n]
+        }
+
+    @ staticmethod
+    def _generate_warped_point(point, homography, height, width, threshold=16):
+        """
+        根据投影变换得到变换后的坐标点，有效关系及不参与负样本搜索的矩阵
+        Args:
+            point: [n,2] 与warped_point一一对应
+            homography: 点对之间的变换关系
+
+        Returns:
+            not_search_mask: [n,n] type为float32的mask,不搜索的位置为1
+        """
+        # 得到投影点的坐标
+        point = np.concatenate((point[:, ::-1], np.ones((point.shape[0], 1))), axis=1)[:, :, np.newaxis]  # [n,3,1]
+        project_point = np.matmul(homography, point)[:, :, 0]
+        project_point = project_point[:, :2] / project_point[:, 2:3]
+        project_point = project_point[:, ::-1]  # 调换为y,x的顺序
+
+        # 投影点在图像范围内的点为有效点，反之则为无效点
+        boarder_0 = np.array((0, 0), dtype=np.float32)
+        boarder_1 = np.array((height-1, width-1), dtype=np.float32)
+        valid_mask = (project_point >= boarder_0) & (project_point <= boarder_1)
+        valid_mask = np.all(valid_mask, axis=1)
+        invalid_mask = ~valid_mask
+
+        # 根据无效点及投影点之间的距离关系确定不搜索的负样本矩阵
+
+        dist = np.linalg.norm(project_point[:, np.newaxis] - project_point[np.newaxis, :], axis=2)
+        not_search_mask = ((dist <= threshold) | invalid_mask[np.newaxis, :]).astype(np.float32)
+        return project_point.astype(np.float32), valid_mask.astype(np.float32), not_search_mask
+
+    def _scale_point_for_sample(self, point):
+        """
+        将点归一化到[-1,1]的区间范围内，并调换顺序为x,y，方便采样
+        Args:
+            point: [n,2] y,x的顺序，原始范围为[0,height-1], [0,width-1]
+        Returns:
+            point: [n,1,2] x,y的顺序，范围为[-1,1]
+        """
+        org_size = np.array((self.height-1, self.width-1), dtype=np.float32)
+        point = ((point * 2. / org_size - 1.)[:, ::-1])[:, np.newaxis, :].copy()
+        return point
+
+    def _random_sample_point(self):
+        """
+        根据预设的输入图像大小，随机均匀采样坐标点
+        """
+        grid = self.fix_grid.copy()
+        # 随机选择指定数目个格子
+
+        point_list = []
+        for i in range(grid.shape[0]):
+            y_start, x_start, y_end, x_end = grid[i]
+            rand_y = np.random.randint(y_start, y_end)
+            rand_x = np.random.randint(x_start, x_end)
+            point_list.append(np.array((rand_y, rand_x), dtype=np.float32))
+        point = np.stack(point_list, axis=0)
+
+        # 取格子的中心当作采样点
+        # point_list = []
+        # for i in range(grid.shape[0]):
+        #     y_start, x_start, y_end, x_end = grid[i]
+        #     y = (y_end - y_start) / 2 + y_start
+        #     x = (x_end - x_start) / 2 + x_start
+        #     point_list.append(np.array((y, x), dtype=np.float32))
+        # point = np.stack(point_list, axis=0)
+
+        return point
+
+    def _generate_fixed_grid(self, x_num=20, y_num=20):
+        """
+        预先采样固定间隔的225个图像格子
+        """
+        grid_y = np.linspace(0, self.height-1, y_num+1, dtype=np.int)
+        grid_x = np.linspace(0, self.width-1, x_num+1, dtype=np.int)
+
+        grid_y_start = grid_y[:y_num].copy()
+        grid_y_end = grid_y[1:y_num+1].copy()
+        grid_x_start = grid_x[:x_num].copy()
+        grid_x_end = grid_x[1:x_num+1].copy()
+
+        grid_start = np.stack((np.tile(grid_y_start[:, np.newaxis], (1, x_num)),
+                               np.tile(grid_x_start[np.newaxis, :], (y_num, 1))), axis=2).reshape((-1, 2))
+        grid_end = np.stack((np.tile(grid_y_end[:, np.newaxis], (1, x_num)),
+                             np.tile(grid_x_end[np.newaxis, :], (y_num, 1))), axis=2).reshape((-1, 2))
+        grid = np.concatenate((grid_start, grid_end), axis=1)
+
+        return grid
+
+    def _convert_points_to_heatmap(self, points):
+        """
+        将原始点位置经下采样后得到heatmap与incmap，heatmap上对应下采样整型点位置处的值为1，其余为0；incmap与heatmap一一对应，
+        在关键点位置处存放整型点到亚像素角点的偏移量，以及训练时用来屏蔽非关键点inc量的incmap_valid
+        Args:
+            points: [n,2]
+
+        Returns:
+            heatmap: [h,w] 关键点位置为1，其余为0
+            incmap: [2,h,w] 关键点位置存放实际偏移，其余非关键点处的偏移量为0
+            incmap_valid: [h,w] 关键点位置为1，其余为0，用于训练时屏蔽对非关键点偏移量的训练，只关注关键点的偏移量
+
+        """
+        height = self.height
+        width = self.width
+
+        # localmap = self.localmap.clone()
+        # padded_heatmap = torch.zeros(
+        #     (height+self.g_paddings*2, width+self.g_paddings*2), dtype=torch.float)
+        heatmap = torch.zeros((height, width), dtype=torch.float)
+
+        num_pt = points.shape[0]
+        if num_pt > 0:
+            for i in range(num_pt):
+                pt = points[i]
+                pt_y_float, pt_x_float = pt
+
+                pt_y_int = round(pt_y_float)
+                pt_x_int = round(pt_x_float)
+
+                pt_y = int(pt_y_int)  # 对真值点位置进行下采样,这里有量化误差
+                pt_x = int(pt_x_int)
+
+                # 排除掉经下采样后在边界外的点
+                if pt_y < 0 or pt_y > height - 1:
+                    continue
+                if pt_x < 0 or pt_x > width - 1:
+                    continue
+
+                # 关键点位置在heatmap上置1，并在incmap上记录该点离亚像素点的偏移量
+                heatmap[pt_y, pt_x] = 1.0
+
+        return heatmap
+
+    def _format_file_list(self):
+        dataset_dir = self.dataset_dir
+        org_image_list = glob.glob(os.path.join(dataset_dir, "*.jpg"))
+        org_image_list = sorted(org_image_list)
+        image_list = []
+        point_list = []
+        for org_image_dir in org_image_list:
+            name = (org_image_dir.split('/')[-1]).split('.')[0]
+            point_dir = os.path.join(dataset_dir, name + '.npy')
+            image_list.append(org_image_dir)
+            point_list.append(point_dir)
+
+        return image_list, point_list
 
 
 class COCOMegPointHeatmapOnlyIndexDataset(COCOMegPointHeatmapTrainDataset):
@@ -1420,14 +1668,14 @@ if __name__ == "__main__":
     np.random.seed(2343)
 
     class Parameters:
-        dataset_dir = "/data/MegPoint/dataset/coco/train2014/images"
-        height = 224
-        width = 224
+        dataset_dir = "/data/MegPoint/dataset/coco/train2014/pseudo_image_points_0"
+        height = 240
+        width = 320
         sample_num = 100
 
     params = Parameters()
-    superpoint_train_dataset = COCOMegPointDescriptorOnlyDataset(params)
-    for i, data in enumerate(superpoint_train_dataset):
+    train_dataset = COCOMegPointHeatmapAllTrainDataset(params)
+    for i, data in enumerate(train_dataset):
         a = 3
 
 
