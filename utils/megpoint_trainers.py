3ea969ee0cc2357b2c6edb8e62cb336bf1d5546a
张宇阳 <zhangyuyang@megvii.com>
2020-01-14 13:17:51 +0800

    [Code Develop] 将loss记录到tensorboard中

--- utils/megpoint_trainers.py
+++ utils/megpoint_trainers.py
@@ -825,6 +825,7 @@ class MegPointHeatmapTrainer(MegPointTrainerTester):
 
                 desp_loss_val = desp_loss.item()
                 loss_val = loss.item()
+                self.summary_writer.add_scalar("loss", loss_val, global_step=int(i+epoch_idx*self.epoch_length))
 
                 self.logger.info(
                     "[Epoch:%2d][Step:%5d:%5d]: loss = %.4f, desp_loss = %.4f"
