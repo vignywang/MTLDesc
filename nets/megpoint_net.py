75ded618d8fbeff78ca0b2e76ed50df1e19cd02a
张宇阳 <zhangyuyang@megvii.com>
2020-01-14 10:33:44 +0800

    [Code Develop] 调整描述子resnet网络结构使之支持3通道图像输入

--- nets/megpoint_net.py
+++ nets/megpoint_net.py
@@ -594,7 +594,7 @@ class ResNet(nn.Module):
                              "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
         self.groups = groups
         self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(1, self.inplanes, kernel_size=7, stride=1, padding=3,
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=1, padding=3,
                                bias=False)
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
