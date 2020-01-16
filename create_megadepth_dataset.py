235c70aaf0fd1f9ab0b35ae07f7624258d4a0460
张宇阳 <zhangyuyang@megvii.com>
2020-01-16 10:12:15 +0800

    [Code Develop] 增加命令行参数和分支使之可选择生成训练集或者验证集

--- create_megadepth_dataset.py
+++ create_megadepth_dataset.py
@@ -11,14 +11,30 @@ args.add_argument("--scenes_ratio", type=float, default=1.0)
 args.add_argument("--pairs_per_scene", type=float, default=0.5)
 args.add_argument("--base_path", type=str, default="/data/MegaDepthOrder")
 args.add_argument("--scene_info_path", type=str, default="/data/MegaDepthOrder/scene_info")
+args.add_argument("--dataset_type", type=str, default="train")
 params = args.parse_args()
 
-creator = MegaDetphDatasetCreator(
-    base_path=params.base_path,
-    scene_info_path=params.scene_info_path,
-    scenes_ratio=params.scenes_ratio,
-    pairs_per_scene=params.pairs_per_scene,
-)
-creator.build_dataset()
+if params.dataset_type == "train":
+    creator = MegaDetphDatasetCreator(
+        base_path=params.base_path,
+        scene_info_path=params.scene_info_path,
+        scenes_ratio=params.scenes_ratio,
+        pairs_per_scene=params.pairs_per_scene,
+        train=True,
+    )
+    creator.build_dataset()
+elif params.dataset_type == "val":
+    creator = MegaDetphDatasetCreator(
+        base_path=params.base_path,
+        scene_info_path=params.scene_info_path,
+        scenes_ratio=params.scenes_ratio,
+        pairs_per_scene=params.pairs_per_scene,
+        train=False,
+    )
+    creator.build_dataset()
+else:
+    print("Unrecognized dataset_type:%s" % params.dataset_type)
+    assert False
+
 
 
