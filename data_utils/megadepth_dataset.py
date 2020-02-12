31fdf13402f8ff587606e970c9303d49bb8f87c7
zhangyuyang <zhangyuyang2016@ia.ac.cn>
2020-02-12 20:08:00 +0800

    [Code Develop] 为数据集megadepth增加数据增强,目前只增强颜色

--- data_utils/megadepth_dataset.py
+++ data_utils/megadepth_dataset.py
@@ -9,6 +9,11 @@ import h5py
 import cv2 as cv
 from tqdm import tqdm
 import numpy as np
+
+import imgaug as ia
+from imgaug import augmenters as iaa
+from imgaug.augmentables.kps import Keypoint, KeypointsOnImage
+
 import torch
 import torch.nn.functional as f
 from torch.utils.data import Dataset
@@ -921,6 +926,63 @@ class MegaDepthDatasetFromPreprocessed(Dataset):
     def __init__(self, dataset_dir):
         self.image_list, self.info_list = self._format_file_list(dataset_dir)
 
+        # 初始化增强算子
+        ia.seed(3212)
+        self.aug_seq = iaa.Sequential(
+            [
+                # apply the following augmenters to most images
+                # iaa.Fliplr(0.5),  # horizontally flip 50% of all images
+                # iaa.Flipud(0.2),  # vertically flip 20% of all images
+                # self.sometimes(iaa.Affine(
+                #     scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                #     # scale images to 80-120% of their size, individually per axis
+                #     translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+                #     # translate by -20 to +20 percent (per axis)
+                #     rotate=(-45, 45),  # rotate by -45 to +45 degrees
+                #     shear=(-16, 16),  # shear by -16 to +16 degrees
+                #     order=[0, 1],  # use nearest neighbour or bilinear interpolation (fast)
+                #     cval=(0, 255),  # if mode is constant, use a cval between 0 and 255
+                #     mode="constant"  # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+                # )),
+                # execute 0 to 5 of the following (less important) augmenters per image
+                # don't execute all of them, as that would often be way too strong
+                iaa.SomeOf(
+                    (0, 5),
+                    [
+                        iaa.OneOf([
+                            iaa.GaussianBlur((0, 3.0)),  # blur images with a sigma between 0 and 3.0
+                            iaa.AverageBlur(k=(2, 7)),  # blur image using local means with kernel sizes between 2 and 7
+                            iaa.MedianBlur(k=(3, 11)),
+                            # blur image using local medians with kernel sizes between 2 and 7
+                        ]),
+                        iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),  # sharpen images
+                        iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05 * 255), per_channel=0.5),
+                        # add gaussian noise to images
+                        iaa.Dropout((0.01, 0.1), per_channel=0.5),  # randomly remove up to 10% of the pixels
+                        iaa.Invert(0.05, per_channel=True),  # invert color channels
+                        iaa.Add((-10, 10), per_channel=0.5),
+                        # change brightness of images (by -10 to 10 of original value)
+                        iaa.AddToHueAndSaturation((-20, 20)),  # change hue and saturation
+                        # either change the brightness of the whole image (sometimes
+                        # per channel) or change the brightness of subareas
+                        iaa.OneOf([
+                            iaa.Multiply((0.5, 1.5), per_channel=0.5),
+                            iaa.BlendAlphaFrequencyNoise(
+                                exponent=(-4, 0),
+                                foreground=iaa.Multiply((0.5, 1.5), per_channel=True),
+                                background=iaa.LinearContrast((0.5, 2.0))
+                            )
+                        ]),
+                        iaa.LinearContrast((0.5, 2.0), per_channel=0.5),  # improve or worsen the contrast
+                        iaa.Grayscale(alpha=(0.0, 1.0)),
+                        # self.sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
+                    ],
+                    random_order=True
+                )
+            ],
+            random_order=True
+        )
+
     def __len__(self):
         return len(self.image_list)
 
@@ -930,12 +992,36 @@ class MegaDepthDatasetFromPreprocessed(Dataset):
 
         image12 = cv.imread(image_dir)[:, :, ::-1].copy()  # 交换BGR为RGB
         image1, image2 = np.split(image12, 2, axis=1)
+        h, w, _ = image1.shape
+        scale = np.array((w-1, h-1), dtype=np.float32)
 
         info = np.load(info_dir)
         desp_point1 = info["desp_point1"]
         desp_point2 = info["desp_point2"]
         valid_mask = info["valid_mask"]
         not_search_mask = info["not_search_mask"]
+
+        desp_point1 = ((desp_point1 + 1.) / 2. * scale)[:, 0, :]
+        desp_point2 = ((desp_point2 + 1.) / 2. * scale)[:, 0, :]
+
+        # augmentation
+        aug_image1, aug_point1 = self.aug_seq(image=image1, keypoints=desp_point1[np.newaxis, :, :])
+        aug_image2, aug_point2 = self.aug_seq(image=image2, keypoints=desp_point2[np.newaxis, :, :])
+
+        desp_point1, desp_point2 = aug_point1[0].copy(), aug_point2[0].copy()
+        image1, image2 = aug_image1.copy(), aug_image2.copy()
+
+        # rescale to (-1,1)
+        desp_point1 = ((desp_point1 / scale) * 2. - 1.)[:, np.newaxis, :]
+        desp_point2 = ((desp_point2 / scale) * 2. - 1.)[:, np.newaxis, :]
+
+        # valid_mask and not_search_mask after augmentation
+        aug_valid_mask = (desp_point1 >= -1.) & (desp_point1 <= 1.) & (desp_point2 >= -1.) & (desp_point2 <= 1.)
+        aug_valid_mask = np.all(aug_valid_mask[:, 0, :], axis=1)
+        valid_mask &= aug_valid_mask
+
+        not_search_point2 = np.all(((desp_point2 < -1.) & (desp_point2 > 1.))[:, 0, :], axis=1)[np.newaxis, :]  # [1,n]
+        not_search_mask |= not_search_point2
 
         # debug use
         # desp_point1 = ((desp_point1 + 1) * 256 / 2.)[:, 0, :]
@@ -973,6 +1059,10 @@ class MegaDepthDatasetFromPreprocessed(Dataset):
             info_list.append(info)
 
         return image_list, info_list
+
+    @staticmethod
+    def sometimes(aug):
+        return iaa.Sometimes(0.5, aug)
 
 
 if __name__ == "__main__":
