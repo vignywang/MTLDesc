8d0daa57c5b26835c5f454feddc97ffd6e30e9d8
zhangyuyang <yyang_zh@163.com>
2020-06-26 16:33:37 +0800

    change adaption code to adapte different dataset

--- adaption_dataset_generation.py
+++ adaption_dataset_generation.py
@@ -1,6 +1,7 @@
 #
 # Created by ZhangYuyang on 2019/8/19
 #
+import os
 import argparse
 
 import numpy as np
@@ -12,44 +13,37 @@ np.random.seed(3242)
 
 def my_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--rounds", type=int, default=0)
-    parser.add_argument("--first_ckpt_file", type=str, default="")
-    parser.add_argument("--second_ckpt_file", type=str, default="")
-    parser.add_argument("--coco_dataset_dir", type=str, required=True)
-    parser.add_argument("--height", type=int, default=240)
-    parser.add_argument("--width", type=int, default=320)
+    parser.add_argument("--ckpt_file", type=str, required=True)
+    parser.add_argument("--dataset_type", type=str, required=True)
+    parser.add_argument("--dataset_dir", type=str, required=True)
+    parser.add_argument("--out_root", type=str, required=True)
     parser.add_argument("--adaption_num", type=int, default=100)
     parser.add_argument("--top_k", type=int, default=300)
     parser.add_argument("--nms_threshold", type=int, default=8)
-    parser.add_argument("--first_threshold", type=float, default=0.005)
-    parser.add_argument("--second_threshold", type=float, default=0.04)
+    # for coco adaption, first round: 0.005, second round: 0.04
+    parser.add_argument("--detection_threshold", type=float, default=0.04)
 
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     params = my_parser()
-    if params.rounds == 0:
-        if params.first_ckpt_file == "":
-            print("Must have first_ckpt_file!")
-            assert False
-        params.ckpt_file = params.first_ckpt_file
-        params.detection_threshold = params.first_threshold
-    elif params.rounds == 1:
-        if params.second_ckpt_file == "":
-            print("Must have second_ckpt_file!")
-            assert False
-        params.ckpt_file = params.second_ckpt_file
-        params.detection_threshold = params.second_threshold
-    else:
-        print("Only support rounds of 0 or 1, current is %d" % params.rounds)
-        assert False
-    params.out_root = params.coco_dataset_dir
 
-    print("rounds: %d" % params.rounds)
-    print("thresholds: %.4f" % params.detection_threshold)
+    print("ckpt_file: %s" % params.ckpt_file)
+    print("dataset_type: %s" % params.dataset_type)
+    print("dataset_dir: %s" % params.dataset_dir)
+    print("out_root: %s" % params.out_root)
+    print("adaption_num: %d" % params.adaption_num)
+    print("top_k: %d" % params.top_k)
+    print("nms_threshold: %d" % params.nms_threshold)
+    print("detection_thresholds: %.4f" % params.detection_threshold)
 
-    adaption_maker = AdaptionMaker(params=params)
+    if not os.path.exists(params.out_root):
+        os.mkdir(params.out_root)
+    # use_gpu = False
+    use_gpu = True
+
+    adaption_maker = AdaptionMaker(params=params, use_gpu=use_gpu)
     adaption_maker.run()
 
 
