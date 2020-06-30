528298b61c9bb2daefa429ccc93b3bf5a575354b
zhangyuyang <yyang_zh@163.com>
2020-06-30 11:08:02 +0800

    add training for ablation study

--- nets/superpoint_net.py
+++ nets/superpoint_net.py
@@ -47,9 +47,6 @@ class BasicSuperPointNet(nn.Module):
         self.convDa = nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
         self.convDb = nn.Conv2d(c5, d1, kernel_size=1, stride=1, padding=0)
 
-        # batch normalization
-        self.bnDa = nn.BatchNorm2d(c5, affine=False)
-
         self.softmax = nn.Softmax(dim=1)
         self.tanh = nn.Tanh()
 
@@ -381,10 +378,35 @@ class SuperPointExtractor128(nn.Module):
         return desp
 
 
-class SuperPointNet(BasicSuperPointNet):
+class SuperPointNet(nn.Module):
 
     def __init__(self):
         super(SuperPointNet, self).__init__()
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        c1, c2, c3, c4, c5, d1 = 64, 64, 128, 128, 256, 256
+        # Shared Encoder.
+        self.conv1a = nn.Conv2d(3, c1, kernel_size=3, stride=1, padding=1)
+        self.conv1b = nn.Conv2d(c1, c1, kernel_size=3, stride=1, padding=1)
+        self.conv2a = nn.Conv2d(c1, c2, kernel_size=3, stride=1, padding=1)
+        self.conv2b = nn.Conv2d(c2, c2, kernel_size=3, stride=1, padding=1)
+        self.conv3a = nn.Conv2d(c2, c3, kernel_size=3, stride=1, padding=1)
+        self.conv3b = nn.Conv2d(c3, c3, kernel_size=3, stride=1, padding=1)
+        self.conv4a = nn.Conv2d(c3, c4, kernel_size=3, stride=1, padding=1)
+        self.conv4b = nn.Conv2d(c4, c4, kernel_size=3, stride=1, padding=1)
+        # Detector Head.
+        self.convPa = nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+        self.convPb = nn.Conv2d(c5, 65, kernel_size=1, stride=1, padding=0)
+        # Descriptor Head.
+        self.convDa = nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+        self.convDb = nn.Conv2d(c5, d1, kernel_size=1, stride=1, padding=0)
+
+        self.softmax = nn.Softmax(dim=1)
+        self.tanh = nn.Tanh()
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
 
     def forward(self, x):
         x = self.relu(self.conv1a(x))
@@ -414,14 +436,14 @@ class SuperPointNet(BasicSuperPointNet):
         return logit, desc, prob
 
 
-class SuperPointDetection(nn.Module):
+class SuperPointDetector(nn.Module):
 
     def __init__(self):
-        super(SuperPointDetection, self).__init__()
+        super(SuperPointDetector, self).__init__()
         self.relu = torch.nn.ReLU(inplace=True)
         self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
         # Shared Encoder.
-        self.conv1a = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
+        self.conv1a = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
         self.conv1b = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.conv2a = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.conv2b = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
@@ -464,14 +486,14 @@ class SuperPointDetection(nn.Module):
         return logit, prob, c1, c2, c3, c4
 
 
-class SuperPointNetDescriptorC1C2C3C4(nn.Module):
+class SuperPointNetDescriptor(nn.Module):
 
     def __init__(self):
-        super(SuperPointNetDescriptorC1C2C3C4, self).__init__()
+        super(SuperPointNetDescriptor, self).__init__()
         self.relu = torch.nn.ReLU(inplace=True)
         self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
         # Shared Encoder.
-        self.conv1a = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
+        self.conv1a = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
         self.conv1b = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.conv2a = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.conv2b = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
@@ -513,186 +535,6 @@ class SuperPointNetDescriptorC1C2C3C4(nn.Module):
         heatmap3 = f.pixel_shuffle(c3, 4)  # [h,w,8]
         heatmap4 = f.pixel_shuffle(c4, 8)  # [h,w,2]
         heatmap = self.heatmap(torch.cat((heatmap1, heatmap2, heatmap3, heatmap4), dim=1))
-
-        # descriptor head
-        cDa = self.relu(self.convDa(c4))
-        feature = self.convDb(cDa)
-
-        dn = torch.norm(feature, p=2, dim=1, keepdim=True)
-        desc = feature.div(dn)
-
-        return heatmap, desc
-
-
-class SuperPointNetDescriptorC2C3C4(nn.Module):
-
-    def __init__(self):
-        super(SuperPointNetDescriptorC2C3C4, self).__init__()
-        self.relu = torch.nn.ReLU(inplace=True)
-        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
-        # Shared Encoder.
-        self.conv1a = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
-        self.conv1b = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.conv2a = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.conv2b = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.conv3a = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.conv3b = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv4a = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv4b = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-
-        self.softmax = nn.Softmax(dim=1)
-
-        # Descriptor Head.
-        self.convDa = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
-        self.convDb = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0)
-
-        self.heatmap = nn.Conv2d((16 + 8 + 2), 1, kernel_size=3, stride=1, padding=1)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-
-    def forward(self, x):
-        x = self.relu(self.conv1a(x))
-        c1 = self.relu(self.conv1b(x))  # 64
-
-        c2 = self.pool(c1)
-        c2 = self.relu(self.conv2a(c2))
-        c2 = self.relu(self.conv2b(c2))  # 64
-
-        c3 = self.pool(c2)
-        c3 = self.relu(self.conv3a(c3))
-        c3 = self.relu(self.conv3b(c3))  # 128
-
-        c4 = self.pool(c3)
-        c4 = self.relu(self.conv4a(c4))
-        c4 = self.relu(self.conv4b(c4))  # 128
-
-        # heatmap1 = c1  # [h,w,64]
-        heatmap2 = f.pixel_shuffle(c2, 2)  # [h,w,16]
-        heatmap3 = f.pixel_shuffle(c3, 4)  # [h,w,8]
-        heatmap4 = f.pixel_shuffle(c4, 8)  # [h,w,2]
-        heatmap = self.heatmap(torch.cat((heatmap2, heatmap3, heatmap4), dim=1))
-
-        # descriptor head
-        cDa = self.relu(self.convDa(c4))
-        feature = self.convDb(cDa)
-
-        dn = torch.norm(feature, p=2, dim=1, keepdim=True)
-        desc = feature.div(dn)
-
-        return heatmap, desc
-
-
-class SuperPointNetDescriptorC3C4(nn.Module):
-
-    def __init__(self):
-        super(SuperPointNetDescriptorC3C4, self).__init__()
-        self.relu = torch.nn.ReLU(inplace=True)
-        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
-        # Shared Encoder.
-        self.conv1a = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
-        self.conv1b = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.conv2a = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.conv2b = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.conv3a = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.conv3b = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv4a = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv4b = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-
-        self.softmax = nn.Softmax(dim=1)
-
-        # Descriptor Head.
-        self.convDa = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
-        self.convDb = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0)
-
-        self.heatmap = nn.Conv2d((8 + 2), 1, kernel_size=3, stride=1, padding=1)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-
-    def forward(self, x):
-        x = self.relu(self.conv1a(x))
-        c1 = self.relu(self.conv1b(x))  # 64
-
-        c2 = self.pool(c1)
-        c2 = self.relu(self.conv2a(c2))
-        c2 = self.relu(self.conv2b(c2))  # 64
-
-        c3 = self.pool(c2)
-        c3 = self.relu(self.conv3a(c3))
-        c3 = self.relu(self.conv3b(c3))  # 128
-
-        c4 = self.pool(c3)
-        c4 = self.relu(self.conv4a(c4))
-        c4 = self.relu(self.conv4b(c4))  # 128
-
-        # heatmap1 = c1  # [h,w,64]
-        # heatmap2 = f.pixel_shuffle(c2, 2)  # [h,w,16]
-        heatmap3 = f.pixel_shuffle(c3, 4)  # [h,w,8]
-        heatmap4 = f.pixel_shuffle(c4, 8)  # [h,w,2]
-        heatmap = self.heatmap(torch.cat((heatmap3, heatmap4), dim=1))
-
-        # descriptor head
-        cDa = self.relu(self.convDa(c4))
-        feature = self.convDb(cDa)
-
-        dn = torch.norm(feature, p=2, dim=1, keepdim=True)
-        desc = feature.div(dn)
-
-        return heatmap, desc
-
-
-class SuperPointNetDescriptorC4(nn.Module):
-
-    def __init__(self):
-        super(SuperPointNetDescriptorC4, self).__init__()
-        self.relu = torch.nn.ReLU(inplace=True)
-        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
-        # Shared Encoder.
-        self.conv1a = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
-        self.conv1b = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.conv2a = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.conv2b = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.conv3a = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.conv3b = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv4a = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv4b = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-
-        self.softmax = nn.Softmax(dim=1)
-
-        # Descriptor Head.
-        self.convDa = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
-        self.convDb = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0)
-
-        self.heatmap = nn.Conv2d(2, 1, kernel_size=3, stride=1, padding=1)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-
-    def forward(self, x):
-        x = self.relu(self.conv1a(x))
-        c1 = self.relu(self.conv1b(x))  # 64
-
-        c2 = self.pool(c1)
-        c2 = self.relu(self.conv2a(c2))
-        c2 = self.relu(self.conv2b(c2))  # 64
-
-        c3 = self.pool(c2)
-        c3 = self.relu(self.conv3a(c3))
-        c3 = self.relu(self.conv3b(c3))  # 128
-
-        c4 = self.pool(c3)
-        c4 = self.relu(self.conv4a(c4))
-        c4 = self.relu(self.conv4b(c4))  # 128
-
-        # heatmap1 = c1  # [h,w,64]
-        # heatmap2 = f.pixel_shuffle(c2, 2)  # [h,w,16]
-        # heatmap3 = f.pixel_shuffle(c3, 4)  # [h,w,8]
-        heatmap4 = f.pixel_shuffle(c4, 8)  # [h,w,2]
-        heatmap = self.heatmap(heatmap4)
 
         # descriptor head
         cDa = self.relu(self.convDa(c4))
