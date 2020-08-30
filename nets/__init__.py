3d0cb927c9c697f20d9faa38f4a80b2b7f9a21c9
zhangyuyang <yyang_zh@163.com>
2020-08-30 17:01:07 +0800

    improve the way to dynamic import

--- nets/__init__.py
+++ nets/__init__.py
@@ -4,7 +4,10 @@
 
 
 def get_model(name):
-    f_name, c_name = name.split('.')
+    # f_name, c_name = name.split('.')
+    names = name.split('.')
+    f_name = '.'.join(names[:-1])
+    c_name = names[-1]
     mod = __import__('{}.{}'.format(__name__, f_name), fromlist=[''])
     return getattr(mod, c_name)
 
