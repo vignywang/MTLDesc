ca01860c2ec83dc92c6ef3f9ec92341975b11143
zhangyuyang <yyang_zh@163.com>
2020-09-07 10:08:21 +0800

    improve label code and generate more robust label results

--- labelers/keypoint_labeler.py
+++ labelers/keypoint_labeler.py
@@ -67,6 +67,68 @@ class _BaseLabeler(object):
     def __exit__(self, *args):
         pass
 
+    def _label(self, image):
+        h, w = image.shape
+
+        images = [image]
+        inv_homographies = [np.eye(3)]
+        for j in range(self.config['adaption_num']):
+            homography = self.homography.sample(height=h, width=w)
+            inv_homography = np.linalg.inv(homography)
+            transformed_image = cv.warpPerspective(image, homography, (w, h), flags=cv.INTER_LINEAR)
+            images.append(transformed_image)
+            inv_homographies.append(inv_homography)
+
+        # 按32一个batch分块
+        batched_image_list = []
+        for i in range(int(len(images)/32)+1):
+            batched_image_list.append(
+                torch.from_numpy(np.stack(images[int(i*32): int((i+1)*32)], axis=0)).unsqueeze(dim=1).to(torch.float))
+
+        prob_list = []
+        for img in batched_image_list:
+            img = img.to(self.device)
+            _, _, prob, _ = self.model(img)
+            prob_list.append(prob)
+
+        # 将概率图展开为原始图像大小
+        prob = torch.cat(prob_list, dim=0)
+        prob = f.pixel_shuffle(prob, 8)
+        prob = prob.detach().cpu().numpy()[:, 0, :, :]  # [n+1, h, w]
+
+        # 分别得到每个预测的概率图反变换的概率图
+        count = np.ones_like(image)
+        counts = []
+        probs = []
+        counts.append(count)
+        for j in range(self.config['adaption_num'] + 1):
+            transformed_prob = cv.warpPerspective(prob[j], inv_homographies[j], (w, h),
+                                                  flags=cv.INTER_LINEAR)
+            transformed_count = cv.warpPerspective(count, inv_homographies[j], (w, h),
+                                                   flags=cv.INTER_NEAREST)
+            probs.append(transformed_prob)
+            counts.append(transformed_count)
+
+        probs = np.stack(probs, axis=2)  # [h,w,n+1]
+        counts = np.stack(counts, axis=2)
+        probs = np.sum(probs, axis=2)
+        counts = np.sum(counts, axis=2)
+        probs = probs / counts
+
+        torch_probs = torch.from_numpy(probs).unsqueeze(dim=0).unsqueeze(dim=0)
+        final_probs = spatial_nms(torch_probs).detach().cpu().numpy()[0, 0]
+
+        satisfied_idx = np.where(final_probs > self.config['detection_threshold'])
+        ordered_satisfied_idx = np.argsort(final_probs[satisfied_idx])[::-1]  # 降序
+        if len(ordered_satisfied_idx) < self.config['top_k']:
+            points = np.stack((satisfied_idx[0][ordered_satisfied_idx],
+                               satisfied_idx[1][ordered_satisfied_idx]), axis=1)
+        else:
+            points = np.stack((satisfied_idx[0][:self.config['top_k']],
+                               satisfied_idx[1][:self.config['top_k']]), axis=1)
+
+        return points
+
 
 class Cocostuff10kLabeler(_BaseLabeler):
 
@@ -83,84 +145,30 @@ class Cocostuff10kLabeler(_BaseLabeler):
             image_id = data['image_id']
             h, w = image.shape
 
-            # resize to 320x320
-            l = 224
-            min_size = min(h, w)
-            image_color = cv.resize(image_color, None, None, fx=l/min_size, fy=l/min_size, interpolation=cv.INTER_LINEAR)
-            image = cv.resize(image, None, None, fx=l/min_size, fy=l/min_size, interpolation=cv.INTER_LINEAR)
-            label = cv.resize(label, None, None, fx=l/min_size, fy=l/min_size, interpolation=cv.INTER_LINEAR)
-            h, w = image.shape
+            # resize and crop to 320
+            l = 320
+            max_size = max(h, w)
+            image_color = cv.resize(image_color, None, None, fx=l/max_size, fy=l/max_size, interpolation=cv.INTER_LINEAR)
+            image = cv.resize(image, None, None, fx=l/max_size, fy=l/max_size, interpolation=cv.INTER_LINEAR)
+            label = cv.resize(label, None, None, fx=l/max_size, fy=l/max_size, interpolation=cv.INTER_NEAREST)
 
-            if h > l:
-                half = int((h-l)/2)
-                image_color = image_color[half: half+l, :, :].copy()
-                image = image[half: half+l, :].copy()
-                label = label[half: half+l, :].copy()
-            elif w > l:
-                half = int((h - l) / 2)
-                image_color = image_color[:, half: half + l, :].copy()
-                image = image[:, half: half + l].copy()
-                label = label[:, half: half + l].copy()
+            # Padding to fit for crop_size
+            h, w = label.shape
+            pad_h = max(l - h, 0)
+            pad_w = max(l - w, 0)
+            pad_kwargs = {
+                "top": 0,
+                "bottom": pad_h,
+                "left": 0,
+                "right": pad_w,
+                "borderType": cv.BORDER_CONSTANT,
+            }
+            if pad_h > 0 or pad_w > 0:
+                image_color = cv.copyMakeBorder(image_color, value=0, **pad_kwargs)
+                image = cv.copyMakeBorder(image, value=0, **pad_kwargs)
+                label = cv.copyMakeBorder(label, value=0, **pad_kwargs)
 
-            images = []
-            inv_homographies = []
-            images.append(image)
-            inv_homographies.append(np.eye(3))
-            for j in range(self.config['adaption_num']):
-                homography = self.homography.sample(height=l, width=l)
-                inv_homography = np.linalg.inv(homography)
-                transformed_image = cv.warpPerspective(image, homography, (l, l),
-                                                       flags=cv.INTER_LINEAR)
-                images.append(transformed_image)
-                inv_homographies.append(inv_homography)
-
-            # 送入网络做统一计算
-            batched_image = torch.from_numpy(np.stack(images, axis=0)).unsqueeze(dim=1).to(torch.float)
-            batched_image_list = [batched_image]
-            if self.config['adaption_num'] > 50:
-                batched_image_list = torch.split(batched_image, [50, self.config['adaption_num']-49], dim=0)
-
-            prob_list = []
-            for img in batched_image_list:
-                img = img.to(self.device)
-                _, _, prob, _ = self.model(img)
-                prob_list.append(prob)
-
-            # 将概率图展开为原始图像大小
-            prob = torch.cat(prob_list, dim=0)
-            prob = f.pixel_shuffle(prob, 8)
-            prob = prob.detach().cpu().numpy()[:, 0, :, :]  # [n+1, h, w]
-
-            # 分别得到每个预测的概率图反变换的概率图
-            count = np.ones_like(image)
-            counts = []
-            probs = []
-            counts.append(count)
-            for j in range(self.config['adaption_num']+1):
-                transformed_prob = cv.warpPerspective(prob[j], inv_homographies[j], (l, l),
-                                                      flags=cv.INTER_LINEAR)
-                transformed_count = cv.warpPerspective(count, inv_homographies[j], (l, l),
-                                                       flags=cv.INTER_NEAREST)
-                probs.append(transformed_prob)
-                counts.append(transformed_count)
-
-            probs = np.stack(probs, axis=2)  # [h,w,n+1]
-            counts = np.stack(counts, axis=2)
-            probs = np.sum(probs, axis=2)
-            counts = np.sum(counts, axis=2)
-            probs = probs/counts
-
-            torch_probs = torch.from_numpy(probs).unsqueeze(dim=0).unsqueeze(dim=0)
-            final_probs = spatial_nms(torch_probs, int(self.config['nms_threshold']*2+1)).detach().cpu().numpy()[0, 0]
-
-            satisfied_idx = np.where(final_probs > self.config['detection_threshold'])
-            ordered_satisfied_idx = np.argsort(final_probs[satisfied_idx])[::-1]  # 降序
-            if len(ordered_satisfied_idx) < self.config['top_k']:
-                points = np.stack((satisfied_idx[0][ordered_satisfied_idx],
-                                   satisfied_idx[1][ordered_satisfied_idx]), axis=1)
-            else:
-                points = np.stack((satisfied_idx[0][:self.config['top_k']],
-                                   satisfied_idx[1][:self.config['top_k']]), axis=1)
+            points = self._label(image)
 
             # debug hyper parameters use
             # image_point = draw_image_keypoints(image, points)
@@ -169,6 +177,4 @@ class Cocostuff10kLabeler(_BaseLabeler):
             cv.imwrite(os.path.join(self.out_root, image_id + '.jpg'), image_color)
             np.save(os.path.join(self.out_root, image_id + '_point'), points)
             np.save(os.path.join(self.out_root, image_id + '_label'), label)
-
-
 
