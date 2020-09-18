7f272bc41026afe8ea5f999127c935cbef1c8105
zhangyuyang <yyang_zh@163.com>
2020-09-18 15:36:15 +0800

    comment the input preprocess

--- nets/monodepth2/resnet_encoder.py
+++ nets/monodepth2/resnet_encoder.py
@@ -86,8 +86,8 @@ class ResnetEncoder(nn.Module):
 
     def forward(self, input_image):
         self.features = []
-        x = (input_image - 0.45) / 0.225
-        x = self.encoder.conv1(x)
+        # x = (input_image - 0.45) / 0.225
+        x = self.encoder.conv1(input_image)
         x = self.encoder.bn1(x)
         self.features.append(self.encoder.relu(x))
         self.features.append(self.encoder.layer1(self.encoder.maxpool(self.features[-1])))
