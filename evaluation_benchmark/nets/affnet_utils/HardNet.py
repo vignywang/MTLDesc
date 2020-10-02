f51b09c8e61426648b6c83ed93a94e688ac99fa7
Changewei <656526171@qq.com>
2020-10-02 15:39:01 +0800

    evaluation benchmark

---
+++ evaluation_benchmark/nets/affnet_utils/HardNet.py
@@ -0,0 +1,101 @@
+import sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.backends.cudnn as cudnn
+import time
+import os
+import math
+import numpy as np
+
+class L2Norm(nn.Module):
+    def __init__(self):
+        super(L2Norm,self).__init__()
+        self.eps = 1e-8
+    def forward(self, x):
+        norm = torch.sqrt(torch.sum(x * x, dim = 1) + self.eps)
+        x= x / norm.unsqueeze(-1).expand_as(x)
+        return x
+
+class L1Norm(nn.Module):
+    def __init__(self):
+        super(L1Norm,self).__init__()
+        self.eps = 1e-10
+    def forward(self, x):
+        norm = torch.sum(torch.abs(x), dim = 1) + self.eps
+        x= x / norm.expand_as(x)
+        return x
+
+class HardTFeatNet(nn.Module):
+    """TFeat model definition
+    """
+
+    def __init__(self, sm):
+        super(HardTFeatNet, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=7),
+            nn.Tanh(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=6),
+            nn.Tanh()
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.1),
+            nn.Conv2d(64, 128, kernel_size=8),
+            nn.Tanh())
+        self.SIFT = sm
+    def input_norm(self,x):
+        flat = x.view(x.size(0), -1)
+        mp = torch.mean(flat, dim=1)
+        sp = torch.std(flat, dim=1) + 1e-7
+        return (x - mp.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1).expand_as(x)) / sp.unsqueeze(-1).unsqueeze(-1).unsqueeze(1).expand_as(x)
+    
+    def forward(self, input):
+        x_features = self.features(self.input_norm(input))
+        x = x_features.view(x_features.size(0), -1)
+        x = self.classifier(x_features)
+        return L2Norm()(x.view(x.size(0), -1))
+
+class HardNet(nn.Module):
+    """HardNet model definition
+    """
+    def __init__(self):
+        super(HardNet, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1, bias = False),
+            nn.BatchNorm2d(32, affine=False),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1, bias = False),
+            nn.BatchNorm2d(32, affine=False),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, bias = False),
+            nn.BatchNorm2d(64, affine=False),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1, bias = False),
+            nn.BatchNorm2d(64, affine=False),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2,padding=1, bias = False),
+            nn.BatchNorm2d(128, affine=False),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1, bias = False),
+            nn.BatchNorm2d(128, affine=False),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(128, 128, kernel_size=8, bias = False),
+            nn.BatchNorm2d(128, affine=False),
+        )
+        #self.features.apply(weights_init)
+
+    def input_norm(self,x):
+        flat = x.view(x.size(0), -1)
+        mp = torch.mean(flat, dim=1)
+        sp = torch.std(flat, dim=1) + 1e-7
+        return (x - mp.detach().unsqueeze(-1).unsqueeze(-1).unsqueeze(-1).expand_as(x)) / sp.detach().unsqueeze(-1).unsqueeze(-1).unsqueeze(1).expand_as(x)
+
+    def forward(self, input):
+        x_features = self.features(self.input_norm(input))
+        x = x_features.view(x_features.size(0), -1)
+        return L2Norm()(x)
